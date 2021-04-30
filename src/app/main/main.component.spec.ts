7ed2e5aa0788291db904b24b99b1bea454ce560c
eng-investor-admin <admin@eng-investor.com>
2021-04-30 11:40:33 +0900

    sono12

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -24,40 +24,67 @@ describe("MainComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  it("should have no data in esppDataList", () => {
-    expect(fixture.componentInstance.esppDataList.length).toEqual(0);
+  it("should have no data", () => {
+    // 準備
+
+    // テストしたい処理
+
+    // 確認
+    expect(component.esppDataList.length).toEqual(0);
   });
 
   it("should copy marketPrice to purchasePrice", () => {
-    fixture.componentInstance.esppForm.get("marketPrice")?.setValue(120);
-    fixture.componentInstance.copyToPurchasePrice()
-    expect(fixture.componentInstance.esppForm.get("purchasePrice")?.value).toEqual(120);
+    // 準備
+    component.esppForm.get("marketPrice")?.setValue(120);
+
+    // テストしたい処理
+    component.copyToPurchasePrice();
+
+    // 確認
+    expect(component.esppForm.get("purchasePrice")?.value).toEqual(120);
   });
 
   it("should not copy marketPrice to purchasePrice if user change purchasePrice value", () => {
+    // 準備
     fixture.componentInstance.esppForm.get("marketPrice")?.setValue(120);
     fixture.componentInstance.esppForm.get("purchasePrice")?.markAsTouched();
-    fixture.componentInstance.copyToPurchasePrice()
-    expect(fixture.componentInstance.esppForm.get("purchasePrice")?.value).toEqual(0);
+
+    // テストしたい処理
+    fixture.componentInstance.copyToPurchasePrice();
+
+    // 確認
+    expect(
+      fixture.componentInstance.esppForm.get("purchasePrice")?.value
+    ).toEqual(0);
   });
 
   it("should save a new espp data", () => {
+    // 準備
     fixture.componentInstance.esppForm.get("name")?.setValue("ABC");
     fixture.componentInstance.esppForm.get("purchaseDate")?.setValue(0);
     fixture.componentInstance.esppForm.get("quantity")?.setValue(10);
     fixture.componentInstance.esppForm.get("marketPrice")?.setValue(120);
     fixture.componentInstance.esppForm.get("purchasePrice")?.setValue(100);
+
+    // テストしたい処理
     fixture.componentInstance.save();
+
+    // 確認
     expect(fixture.componentInstance.esppDataList.length).toEqual(1);
   });
 
   it("should calculate profit", () => {
+    // 準備
     fixture.componentInstance.esppForm.get("name")?.setValue("ABC");
     fixture.componentInstance.esppForm.get("purchaseDate")?.setValue(0);
     fixture.componentInstance.esppForm.get("quantity")?.setValue(10);
     fixture.componentInstance.esppForm.get("marketPrice")?.setValue(120);
     fixture.componentInstance.esppForm.get("purchasePrice")?.setValue(100);
+
+    // テストしたい処理
     fixture.componentInstance.save();
+
+    // 確認
     expect(fixture.componentInstance.esppDataList[0].profit).toEqual(200);
   });
 });
